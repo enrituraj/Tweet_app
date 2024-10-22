64ac781381d4d22ba5a70cb16c7a672b0ad27958
Ritu <rn26rituraj@gmail.com>
2024-10-22 17:51:06 +0530

    fixing issues

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
 echo " BUILD START"
-python3.9  -m pip install -r requirements.txt
-python3.9 chaiheadq/manage.py collectstatic  --noinput --clear
+
+# Ensure pip is installed
+python3.9 -m ensurepip --upgrade
+
+# Install the dependencies
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files in the required directory
+python3.9 chaiheadq/manage.py collectstatic --noinput --clear --settings=your_project_name.settings
+
 echo " BUILD END"
