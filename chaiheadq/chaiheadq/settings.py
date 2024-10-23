797f639e7b90f5662bdf14f8b2dd1de5fb87bd4c
Ritu <rn26rituraj@gmail.com>
2024-10-23 07:05:54 +0530

    connecting to postgress database

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -79,10 +80,9 @@ WSGI_APPLICATION = 'chaiheadq.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('POSTGRES_URL')
+    )
 }
 
 
