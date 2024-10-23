d8b85b4a16590a4e3d54bac6baa7810fd2f65e67
Ritu <rn26rituraj@gmail.com>
2024-10-23 06:31:28 +0530

    configure for deployment

--- chaiheadq/build_files.sh
+++ chaiheadq/build_files.sh
@@ -9,10 +9,9 @@ python3.9 -m ensurepip --upgrade
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
-cd chaiheadq/
 # Collect static files in the required directory
 python3.9 manage.py collectstatic --noinput --clear --settings=chaiheadq.settings
 mkdir -p staticfiles_build
 mv staticfiles/* staticfiles_build/
-cd ..
+
 echo " BUILD END"
